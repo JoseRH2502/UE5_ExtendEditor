5d969984a68999b6cecefb3023bf4a01773a55a5
JoseramirezFPL <138727924+JoseramirezFPL@users.noreply.github.com>
2025-02-22 20:53:05 -0600

    Preparing for material creations

---
+++ Plugins/SuperManager/Source/SuperManager/Private/AssetAction/QuickMaterialCreationWidget.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AssetAction/QuickMaterialCreationWidget.h"
+
+#include "DebugHeader.h"
+
+#pragma region QuickMaterialCreationCore
+
+void UQuickMaterialCreationWidget::CreateMaterialFromSelectedTextures()
+{
+	if(bCustomMaterialName)
+	{
+		if(MaterialName.IsEmpty() || MaterialName.Equals(TEXT("M_")))
+		{
+			DebugHeader::ShowMsgDialog(EAppMsgType::Ok,TEXT("Please enter a valid name"));
+			return;
+		}
+	}
+}
+
+#pragma endregion
