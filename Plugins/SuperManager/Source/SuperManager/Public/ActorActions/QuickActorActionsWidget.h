b974c54589854c82e22eff83d6b89a7458bcefc4
JoseramirezFPL <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2025-03-03 23:27:38 -0600

    Duplicate Actor tool

---
+++ Plugins/SuperManager/Source/SuperManager/Public/ActorActions/QuickActorActionsWidget.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "QuickActorActionsWidget.generated.h"
+
+/**
+ * 
+ */
+
+UENUM(BlueprintType)
+enum class E_DuplicationAxis : uint8
+{
+	EDA_XAxis UMETA (DisplayName = "X Axis"),
+	EDA_YAxis UMETA (DisplayName = "Y Axis"),
+	EDA_ZAxis UMETA (DisplayName = "Z Axis"),
+	EDA_MAX UMETA (DisplayName = "Default Max")
+};
+
+UCLASS()
+class SUPERMANAGER_API UQuickActorActionsWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SelectAllActorsWithSimilarName();
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "ActorBatchSelection")
+	TEnumAsByte<ESearchCase::Type> SearchCase = ESearchCase::IgnoreCase;
+
+#pragma region ActorBatchDuplication
+
+	UFUNCTION(BlueprintCallable)
+	void DuplicateActors();
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "ActorBatchDuplication")
+	E_DuplicationAxis AxisForDuplication = E_DuplicationAxis::EDA_XAxis;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "ActorBatchDuplication")
+	int32 NumberOfDuplicates = 5;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "ActorBatchDuplication")
+	float OffsetDist = 300.f;
+
+#pragma endregion
+private:
+	UPROPERTY()
+	class UEditorActorSubsystem* EditorActorSubsystem;
+
+	bool GetEditorActorSubsystem();
+};
